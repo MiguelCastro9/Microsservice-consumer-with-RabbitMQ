09656c98e9ab969f20d3a7d78e54d73c8f9e3a5c
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-07-08 15:29:19 -0300

    criando o consumer para consumir as mesagens enviadas

---
+++ consumer/src/main/java/com/consumer/consumers/ProdutoConsumer.java
@@ -0,0 +1,24 @@
+package com.consumer.consumers;
+
+import com.produto.constants.RabbitMQConstants;
+import com.produto.dto.ProdutoDto;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Miguel Castro
+ */
+@Component
+public class ProdutoConsumer {
+
+    // ANOTAÇÃO QUE PARA ESCUTAR AS MENSAGEM QUE SÃO ENVIADAS AO RABBITMQ
+    @RabbitListener(queues = RabbitMQConstants.FILA_PRODUTO)
+    private void consumidor(ProdutoDto produtoDto) {
+        System.out.println("--- PRODUTO ---");
+        System.out.println(produtoDto.codigo_produto);
+        System.out.println(produtoDto.nome_produto);
+        System.out.println(produtoDto.valor_produto);
+        System.out.println(produtoDto.quantidade_produto);
+    }
+}
